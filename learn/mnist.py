163c303cd8e6d6d416b20e990dbe47a6a884ef23
RoyPy <bogush.vano@gmail.com>
2017-12-05 20:37:42 +0700

    Learn through simple mnist example of handwritten digit recognition

---
+++ learn/mnist.py
@@ -0,0 +1,38 @@
+# silence the warning about unused CPU instructions
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+sess = tf.InteractiveSession()
+
+# 28 * 28 = 784 pixels images - input tensor
+x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
+# 10 digits - output tensor
+y_ = tf.placeholder(dtype=tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.matmul(x, W) + b
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+)
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+for _ in range(1000):
+    batch = mnist.train.next_batch(batch_size=100)
+    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
