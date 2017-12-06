5a81b4979504e5ac1c5f5f2457032e32f42d0d27
RoyPy <bogush.vano@gmail.com>
2017-12-06 13:10:30 +0700

    Remove unused statement

--- learn/mnist.py
+++ learn/mnist.py
@@ -4,7 +4,6 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
-import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
